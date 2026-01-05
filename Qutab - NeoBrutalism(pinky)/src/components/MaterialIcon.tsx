6004b261165a08d15fed1199630aad2742e619d1
BlackCroissant450 <mohammad.hanif@student.president.ac.id>
2026-01-05 13:39:06 +0700

    preperation

--- Qutab - NeoBrutalism(pinky)/src/components/MaterialIcon.tsx
+++
@@ -1,8 +0,0 @@
-interface MaterialIconProps {
-  icon: string;
-  className?: string;
-}
-
-export default function MaterialIcon({ icon, className = '' }: MaterialIconProps) {
-  return <span className={`material-symbols-outlined ${className}`}>{icon}</span>;
-}
